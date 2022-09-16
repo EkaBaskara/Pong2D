2fc0d9df19274e88e43a06fd4c7893e225c4e3f5
EkaBaskara <ekabaskara2000@gmail.com>
2022-09-16 16:35:02 +0700

    Gameplay Pong2D
    Kodinngan untuk Gameplay Pong2D

---
+++ Assets/Script/BallController.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BallController : MonoBehaviour
+{
+    public int force;
+    Rigidbody2D rigid;
+    int scoreP1;
+    int scoreP2;
+    TextMeshProUGUI scoreUIP1;
+    TextMeshProUGUI scoreUIP2;
+    GameObject panelSelesai;
+    TextMeshProUGUI txPemenang;
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        Vector2 arah = new Vector2(2, 0).normalized;
+        rigid.AddForce(arah * force);
+        scoreP1 = 0;
+        scoreP2 = 0;
+        scoreUIP1 = GameObject.Find("Score1").GetComponent<TextMeshProUGUI>();
+        scoreUIP2 = GameObject.Find("Score2").GetComponent<TextMeshProUGUI>();
+        panelSelesai = GameObject.Find("PanelSelesai");
+        panelSelesai.SetActive(false);
+    }
+    //Upadate is called once per frame
+    void Update()
+    {
+    }
+    
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.name == "TepiKanan")
+        {
+            scoreP1 += 1;
+            TampilkanScore();
+            if (scoreP1 == 5)
+            {
+                panelSelesai.SetActive(true);
+                txPemenang = GameObject.Find("Pemenang").GetComponent<TextMeshProUGUI>();
+                txPemenang.text = "Player Biru Pemenang";
+                Destroy(gameObject);
+                return;
+            }
+            ResetBall();
+            Vector2 arah = new Vector2(2, 0).normalized;
+            rigid.AddForce(arah * force);
+        }
+        if (coll.gameObject.name == "TepiKiri")
+        {
+            scoreP2 += 1;
+            TampilkanScore();
+            if (scoreP2 == 5)
+            {
+                panelSelesai.SetActive(true);
+                txPemenang = GameObject.Find("Pemenang").GetComponent<TextMeshProUGUI>();
+                txPemenang.text = "Player Merah Pemenang";
+                Destroy(gameObject);
+                return;
+            }
+            ResetBall();
+            Vector2 arah = new Vector2(-2, 0).normalized;
+            rigid.AddForce(arah * force);
+        }
+        if (coll.gameObject.name == "Pemukul 1" || coll.gameObject.name == "Pemukul 2")
+        {
+            float sudut = (transform.position.y - coll.transform.position.y) * 5f;
+            Vector2 arah = new Vector2(rigid.velocity.x,sudut).normalized;
+            rigid.velocity = new Vector2(0, 0);
+            rigid.AddForce(arah * force * 2);
+        }
+    }
+    void ResetBall()
+    {
+    transform.localPosition = new Vector2(0,0);
+    rigid.velocity = new Vector2(0,0);
+    }
+    void TampilkanScore()
+    {
+        Debug.Log("Score P1:" + scoreP1 + "ScoreP2" + scoreP2);
+        scoreUIP1.text = scoreP1 + "";
+        scoreUIP2.text = scoreP2 + "";
+    }
+}
